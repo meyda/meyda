96e9feca9accd2ecbf776f08afa77045a67312d2
hughrawlinson <hugh@codeoclock.net>
2014-08-12 00:43:36 +0100

    made reduce work, lengthens time it works for

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 var Medya = function(audioContext,callback,bufferSize=256){
 	var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
 	node.onaudioprocess = function(e) {
+		// type float32Array
 		var input = e.inputBuffer.getChannelData(0);
-		var output = e.outputBuffer.getChannelData(0);
-		console.log("isArray"+input.isArray())
+
+		// Convert from float32Array to Array
+		input = Array.prototype.slice.call(input);
 		callback(Math.sqrt(input.reduce(function(last,current){
 			return Math.pow(current,2);
 		},0)/bufferSize))
