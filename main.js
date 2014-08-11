4d57dbf8c5a0549ddfb38eb510b9de468cd5ffbf
hughrawlinson <hugh@codeoclock.net>
2014-08-12 00:23:43 +0100

    moved library back to include file

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// Meyda Javascript DSP library
-function Meyda(audioContext,callback,bufferSize=256){
+// Meyda Javascript DSP library 
+var Medya = function(audioContext,callback,bufferSize=256){
 	var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
-    node.onaudioprocess = function(e) {
-        var input = e.inputBuffer.getChannelData(0);
-        var output = e.outputBuffer.getChannelData(0);
-        var tally = 0;
-        for (var x = 0; x < input.length; x++){
-        	tally+=Math.pow(input[x],2);
-        }
-        callback(Math.sqrt(1/bufferSize*tally))
-    }
-    return node;
+	node.onaudioprocess = function(e) {
+		var input = e.inputBuffer.getChannelData(0);
+		var output = e.outputBuffer.getChannelData(0);
+		var tally = 0;
+		for (var x = 0; x < input.length; x++){
+			tally+=Math.pow(input[x],2);
+		}
+		callback(Math.sqrt(1/bufferSize*tally))
+	}
+	return node;
 }
