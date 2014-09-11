509c22227b96d99c3d4bc4568c4f49ca25a12ef2
Jakub Fiala <mu201jf@gold.ac.uk>
2014-09-11 17:02:57 +0200

    energy + spectrum added
    decided to only call it spectrum because we can’t get phase spectrum
    anyway (yet). Audio broken again in firefox.
    
    
    Former-commit-id: 68fb217f5ce6f624ee8c80dd6ad9343108c72034

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 // Meyda Javascript DSP library
 var featureExtractors = {
-	"rms": function(input,bufferSize){
+	"rms": function(input, bufferSize, _analyser){
 		return Math.sqrt(input.reduce(function(last,current){
 			return Math.pow(current,2);
 		},0)/bufferSize)
 	},
-	"energy": function(input, bufferSize) {
+	"energy": function(input, bufferSize, _analyser) {
 		return input.reduce(function(prev, cur) {
 			return prev + Math.pow(Math.abs(cur),2);
 		}, 0);
+	},
+	"spectrum": function(input, bufferSize, _analyser) {
+		var s = new Float32Array;
+		_analyser.getFloatFrequencyData(s);
+		return s;
 	}
+
 }
 
 var Meyda = function(audioContext,callback,feature,bufferSize){
-	var node = audioContext.createScriptProcessor(bufferSize, 1, 1);
-	node.onaudioprocess = function(e) {
+	var processor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+	var analyser = audioContext.createAnalyser();
+	analyser.fftSize = bufferSize;
+	processor.onaudioprocess = function(e) {
 		// type float32Array
 		var input = e.inputBuffer.getChannelData(0);
 
@@ -22,15 +30,16 @@ var Meyda = function(audioContext,callback,feature,bufferSize){
 		input = Array.prototype.slice.call(input);
 		if(typeof feature === "object"){
 			for (var x = 0; x < feature.length; x++){
-				callback(featureExtractors[feature[x]](input,bufferSize));
+				callback(featureExtractors[feature[x]](input, bufferSize, analyser));
 			}
 		}
 		else if (typeof feature === "string"){
-			callback(featureExtractors[feature](input,bufferSize));
+			callback(featureExtractors[feature](input, bufferSize, analyser));
 		}
 		else{
 			throw "Invalid Feature Format";
 		}
 	}
-	return node;
+	analyser.connect(processor);
+	return analyser;
 }
