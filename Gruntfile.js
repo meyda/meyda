6fecdc0da3ee83be147ada7a0ecf3fa253341726
hughrawlinson <hugh@codeoclock.net>
2015-01-28 10:56:27 +0100

    adds initial build system

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['meyda.js','lib/jsfft/*.js'],
+        // the location of the resulting JS file
+        dest: 'tmp/meyda.concat.js'
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'meyda.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    clean: ['tmp']
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.registerTask('default', ['concat','uglify','clean'])
+
+};
