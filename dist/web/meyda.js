7c699c1e1a902c2dca20c897e48d75be4cfbd6dd
Hugh Rawlinson <hugh@spotify.com>
2016-03-28 20:27:27 +0200

    Buffer Comparison Support
    Adds support for comparing the current buffer (and associated data) with
    that of the previous frame.

--- dist/web/meyda.js
+++ dist/web/meyda.js
@@ -2147,24 +2147,18 @@ var Meyda = {
       this.signal = signal;
     }
 
-    var windowedSignal = utilities.applyWindow(this.signal, this.windowingFunction);
+    var preparedSignal = prepareSignalWithSpectrum(signal, this.windowingFunction, this.bufferSize);
 
-    // create complexarray to hold the spectrum
-    var data = new complexArray.ComplexArray(this.bufferSize);
+    this.signal = preparedSignal.windowedSignal;
+    this.complexSpectrum = preparedSignal.complexSpectrum;
+    this.ampSpectrum = preparedSignal.ampSpectrum;
 
-    // map time domain
-    data.map(function (value, i, n) {
-      value.real = windowedSignal[i];
-    });
+    if (previousSignal) {
+      var _preparedSignal = prepareSignalWithSpectrum(prevousSignal, this.windowingFunction, this.bufferSize);
 
-    // transform
-    var spec = data.FFT();
-
-    // assign to meyda
-    this.complexSpectrum = spec;
-    this.ampSpectrum = new Float32Array(this.bufferSize / 2);
-    for (var i = 0; i < this.bufferSize / 2; i++) {
-      this.ampSpectrum[i] = Math.sqrt(Math.pow(spec.real[i], 2) + Math.pow(spec.imag[i], 2));
+      this.previousSignal = _preparedSignal.windowedSignal;
+      this.previousComplexSpectrum = _preparedSignal.complexSpectrum;
+      this.previousAmpSpectrum = _preparedSignal.ampSpectrum;
     }
 
     if ((typeof feature === 'undefined' ? 'undefined' : _typeof(feature)) === 'object') {
@@ -2177,7 +2171,10 @@ var Meyda = {
           bufferSize: this.bufferSize,
           sampleRate: this.sampleRate,
           barkScale: this.barkScale,
-          melFilterBank: this.melFilterBank
+          melFilterBank: this.melFilterBank,
+          previousSignal: this.previousSignal,
+          previousAmpSpectrum: this.previousAmpSpectrum,
+          previousComplexSpectrum: this.previousComplexSpectrum
         });
       }
 
@@ -2190,12 +2187,44 @@ var Meyda = {
         bufferSize: this.bufferSize,
         sampleRate: this.sampleRate,
         barkScale: this.barkScale,
-        melFilterBank: this.melFilterBank
+        melFilterBank: this.melFilterBank,
+        previousSignal: this.previousSignal,
+        previousAmpSpectrum: this.previousAmpSpectrum,
+        previousComplexSpectrum: this.previousComplexSpectrum
       });
     } else {
       throw this._errors.invalidFeatureFmt;
     }
   }
+};
+
+var prepareSignalWithSpectrum = function prepareSignalWithSpectrum(signal, windowingFunction, bufferSize) {
+  var preparedSignal = {};
+
+  if (typeof signal.buffer == 'undefined') {
+    //signal is a normal array, convert to F32A
+    preparedSignal.signal = utilities.arrayToTyped(signal);
+  } else {
+    preparedSignal.signal = signal;
+  }
+
+  preparedSignal.windowedSignal = utilities.applyWindow(preparedSignal, windowingFunction);
+
+  // create complexarray to hold the spectrum
+  var data = new complexArray.ComplexArray(this.bufferSize);
+
+  // map time domain
+  data.map(function (value, i, n) {
+    value.real = windowedSignal[i];
+  });
+
+  preparedSignal.complexSpectrum = data.FFT();
+  preparedSignal.ampSpectrum = new Float32Array(bufferSize / 2);
+  for (var i = 0; i < this.bufferSize / 2; i++) {
+    preparedSignal.ampSpectrum[i] = Math.sqrt(Math.pow(spec.real[i], 2) + Math.pow(spec.imag[i], 2));
+  }
+
+  return preparedSignal;
 };
 
 exports.default = Meyda;
@@ -2206,11 +2235,6 @@ module.exports = exports['default'];
 
 },{"./featureExtractors":26,"./meyda-wa":28,"./utilities":29,"jsfft":6,"jsfft/lib/complex_array":5}],28:[function(require,module,exports){
 'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.MeydaAnalyzer = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -2226,79 +2250,87 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var MeydaAnalyzer = exports.MeydaAnalyzer = function () {
-    function MeydaAnalyzer(options, _this) {
-        _classCallCheck(this, MeydaAnalyzer);
+var MeydaAnalyzer = function () {
+  function MeydaAnalyzer(options, _this) {
+    var _this2 = this;
 
-        this._m = _this;
-        if (!options.audioContext) throw this._m._errors.noAC;else if (options.bufferSize && !utilities.isPowerOfTwo(options.bufferSize)) throw this._m._errors.notPow2;else if (!options.source) throw this._m._errors.noSource;
+    _classCallCheck(this, MeydaAnalyzer);
 
-        this._m.audioContext = options.audioContext;
+    this._m = _this;
+    if (!options.audioContext) throw self._errors.noAC;else if (options.bufferSize && !utilities.isPowerOfTwo(options.bufferSize)) throw self._errors.notPow2;else if (!options.source) throw self._errors.noSource;
 
-        // TODO: validate options
-        this._m.bufferSize = options.bufferSize || _this.bufferSize || 256;
-        this._m.sampleRate = options.sampleRate || this._m.audioContext.sampleRate || 44100;
-        this._m.callback = options.callback;
-        this._m.windowingFunction = options.windowingFunction || 'hanning';
-        this._m.featureExtractors = featureExtractors;
-        this._m.EXTRACTION_STARTED = options.startImmediately || false;
+    this._m.audioContext = options.audioContext;
 
-        //create nodes
-        this._m.spn = this._m.audioContext.createScriptProcessor(this._m.bufferSize, 1, 1);
-        this._m.spn.connect(this._m.audioContext.destination);
+    // TODO: validate options
+    this._m.setSource(options.source);
+    this._m.bufferSize = options.bufferSize || this._m.bufferSize || 256;
+    this._m.sampleRate = options.sampleRate || this._m.audioContext.sampleRate || 44100;
+    this._m.callback = options.callback;
+    this._m.windowingFunction = options.windowingFunction || 'hanning';
+    this._m.featureExtractors = featureExtractors;
+    this._m.EXTRACTION_STARTED = options.startImmediately || false;
 
-        this._m._featuresToExtract = options.featureExtractors || [];
+    //create nodes
+    this._m.spn = this._m.audioContext.createScriptProcessor(this._m.bufferSize, 1, 1);
+    this._m.spn.connect(this._m.audioContext.destination);
 
-        //always recalculate BS and MFB when a new Meyda analyzer is created.
-        this._m.barkScale = utilities.createBarkScale(this._m.bufferSize, this._m.sampleRate, this._m.bufferSize);
-        this._m.melFilterBank = utilities.createMelFilterBank(this._m.melBands, this._m.sampleRate, this._m.bufferSize);
+    this._m._featuresToExtract = options.featureExtractors || [];
 
-        this._m.inputData = null;
+    //always recalculate BS and MFB when a new Meyda analyzer is created.
+    this._m.barkScale = utilities.createBarkScale(this._m.bufferSize, this._m.sampleRate, this._m.bufferSize);
+    this._m.melFilterBank = utilities.createMelFilterBank(this._m.melBands, this._m.sampleRate, this._m.bufferSize);
 
-        _this = this;
+    this._m.inputData = null;
+    this._m.previousInputData = null;
 
-        this.setSource(options.source);
+    _this = this;
 
-        this._m.spn.onaudioprocess = function (e) {
-            _this._m.inputData = e.inputBuffer.getChannelData(0);
+    this.setSource(options.source);
 
-            var features = _this._m.extract(_this._m._featuresToExtract, _this._m.inputData);
+    this._m.spn.onaudioprocess = function (e) {
+      if (_this2._m.inputData !== null) {
+        _this2._m.previousInputData = _this2._m.inputData;
+      }
 
-            // call callback if applicable
-            if (typeof _this._m.callback === 'function' && _this._m.EXTRACTION_STARTED) {
-                _this._m.callback(features);
-            }
-        };
+      _this2._m.inputData = e.inputBuffer.getChannelData(0);
+
+      var features = _this2._m.extract(_this2._m._featuresToExtract, _this2._m.inputData, _this2._m.previousInputData);
+
+      // call callback if applicable
+      if (typeof _this2._m.callback === 'function' && _this2._m.EXTRACTION_STARTED) {
+        _this2._m.callback(features);
+      }
+    };
+  }
+
+  _createClass(MeydaAnalyzer, [{
+    key: 'start',
+    value: function start(features) {
+      this._featuresToExtract = features;
+      this.EXTRACTION_STARTED = true;
     }
+  }, {
+    key: 'stop',
+    value: function stop() {
+      this.EXTRACTION_STARTED = false;
+    }
+  }, {
+    key: 'setSource',
+    value: function setSource(source) {
+      source.connect(this.spn);
+    }
+  }, {
+    key: 'get',
+    value: function get(features) {
+      if (self.inputData !== null) {
+        return self.extract(features || self._featuresToExtract, self.inputData, self.previousInputData);
+      } else {
+        return null;
+      }
+    }
+  }]);
 
-    _createClass(MeydaAnalyzer, [{
-        key: 'start',
-        value: function start(features) {
-            this._m._featuresToExtract = features;
-            this._m.EXTRACTION_STARTED = true;
-        }
-    }, {
-        key: 'stop',
-        value: function stop() {
-            this._m.EXTRACTION_STARTED = false;
-        }
-    }, {
-        key: 'setSource',
-        value: function setSource(source) {
-            source.connect(this._m.spn);
-        }
-    }, {
-        key: 'get',
-        value: function get(features) {
-            if (this._m.inputData !== null) {
-                return this._m.extract(features || this._m._featuresToExtract, this._m.inputData);
-            } else {
-                return null;
-            }
-        }
-    }]);
-
-    return MeydaAnalyzer;
+  return MeydaAnalyzer;
 }();
 
 },{"./featureExtractors":26,"./utilities":29}],29:[function(require,module,exports){
--- src/meyda-wa.js
+++ src/meyda-wa.js
@@ -1,83 +1,94 @@
 import * as utilities from './utilities';
 import * as featureExtractors from './featureExtractors';
 
-export class MeydaAnalyzer{
-    constructor(options, _this) {
-      this._m = _this;
-      if (!options.audioContext)
-          throw this._m._errors.noAC;
-      else if (options.bufferSize &&
-          !utilities.isPowerOfTwo(options.bufferSize))
-          throw this._m._errors.notPow2;
-      else if (!options.source)
-          throw this._m._errors.noSource;
+class MeydaAnalyzer{
+	constructor(options, _this) {
+  this._m = _this;
+  if (!options.audioContext)
+  throw self._errors.noAC;
+  else if (options.bufferSize &&
+        !utilities.isPowerOfTwo(options.bufferSize))
+  throw self._errors.notPow2;
+  else if (!options.source)
+  throw self._errors.noSource;
 
-      this._m.audioContext = options.audioContext;
+  this._m.audioContext = options.audioContext;
 
-      // TODO: validate options
-      this._m.bufferSize = options.bufferSize || _this.bufferSize || 256;
-      this._m.sampleRate = options.sampleRate ||
-          this._m.audioContext.sampleRate ||
-              44100;
-      this._m.callback = options.callback;
-      this._m.windowingFunction = options.windowingFunction || 'hanning';
-      this._m.featureExtractors = featureExtractors;
-      this._m.EXTRACTION_STARTED = options.startImmediately || false;
+  // TODO: validate options
+  this._m.setSource(options.source);
+  this._m.bufferSize = options.bufferSize || this._m.bufferSize || 256;
+  this._m.sampleRate = options.sampleRate
+    || this._m.audioContext.sampleRate || 44100;
+  this._m.callback = options.callback;
+  this._m.windowingFunction = options.windowingFunction || 'hanning';
+  this._m.featureExtractors = featureExtractors;
+  this._m.EXTRACTION_STARTED = options.startImmediately || false;
 
-      //create nodes
-      this._m.spn = this._m.audioContext.createScriptProcessor(
-          this._m.bufferSize, 1, 1);
-      this._m.spn.connect(this._m.audioContext.destination);
+  //create nodes
+  this._m.spn = this._m.audioContext.createScriptProcessor(
+    this._m.bufferSize, 1, 1);
+  this._m.spn.connect(this._m.audioContext.destination);
 
-      this._m._featuresToExtract = options.featureExtractors || [];
+  this._m._featuresToExtract = options.featureExtractors || [];
 
-      //always recalculate BS and MFB when a new Meyda analyzer is created.
-      this._m.barkScale = utilities.createBarkScale(
-          this._m.bufferSize, this._m.sampleRate, this._m.bufferSize);
-      this._m.melFilterBank = utilities.createMelFilterBank(
-          this._m.melBands, this._m.sampleRate, this._m.bufferSize);
+  //always recalculate BS and MFB when a new Meyda analyzer is created.
+  this._m.barkScale = utilities.createBarkScale(
+    this._m.bufferSize,
+    this._m.sampleRate,
+    this._m.bufferSize);
+  this._m.melFilterBank = utilities.createMelFilterBank(
+    this._m.melBands,
+    this._m.sampleRate,
+    this._m.bufferSize);
 
-      this._m.inputData = null;
+  this._m.inputData = null;
+  this._m.previousInputData = null;
 
-      _this = this;
+  _this = this;
 
-      this.setSource(options.source);
+  this.setSource(options.source);
 
-      this._m.spn.onaudioprocess = function (e) {
-        _this._m.inputData = e.inputBuffer.getChannelData(0);
-
-        var features = _this._m.extract(
-            _this._m._featuresToExtract, _this._m.inputData);
-
-        // call callback if applicable
-        if (typeof _this._m.callback === 'function' &&
-                _this._m.EXTRACTION_STARTED) {
-          _this._m.callback(features);
-        }
-
-      };
+  this._m.spn.onaudioprocess = (e)=> {
+    if (this._m.inputData !== null) {
+      this._m.previousInputData = this._m.inputData;
     }
 
-    start(features) {
-      this._m._featuresToExtract = features;
-      this._m.EXTRACTION_STARTED = true;
+    this._m.inputData = e.inputBuffer.getChannelData(0);
+
+    var features = this._m.extract(
+      this._m._featuresToExtract,
+      this._m.inputData,
+      this._m.previousInputData);
+
+    // call callback if applicable
+    if (typeof this._m.callback === 'function' && this._m.EXTRACTION_STARTED) {
+      this._m.callback(features);
     }
 
-    stop() {
-      this._m.EXTRACTION_STARTED = false;
-    }
+  };
+	}
 
-    setSource(source) {
-      source.connect(this._m.spn);
-    }
+	start(features) {
+  this._featuresToExtract = features;
+  this.EXTRACTION_STARTED = true;
+	}
 
-    get(features) {
-      if (this._m.inputData !== null) {
-        return this._m.extract(
-            (features || this._m._featuresToExtract), this._m.inputData
-        );
-      } else {
-        return null;
-      }
-    }
+	stop() {
+  this.EXTRACTION_STARTED = false;
+	}
+
+	setSource(source) {
+  source.connect(this.spn);
+	}
+
+	get(features) {
+  if (self.inputData !== null) {
+    return self.extract(
+            (features || self._featuresToExtract),
+            self.inputData,
+            self.previousInputData);
+  } else {
+    return null;
+  }
+	}
 }
