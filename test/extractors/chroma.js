df0835001165d787e6b6c4349b474a3467be7632
Oscar Dub <oscar@roli.com>
2017-08-16 12:46:59 +0100

    Move test data into separate file.

--- test/extractors/chroma.js
+++ test/extractors/chroma.js
@@ -15,23 +15,8 @@ describe('chroma', function () {
       chromaFilterBank:utilities.createChromaFilterBank(12, 44100, 512),
     });
 
-    const expectedValues = [
-      0.889173501932042,
-      0.8377149437424725,
-      0.7983608438722133,
-      0.8340835109903056,
-      0.9257658376982487,
-      0.9697310997078421,
-      1,
-      0.8781674865696717,
-      0.7889747445705728,
-      0.7880426774703064,
-      0.9250473589362254,
-      0.8824577772499042,
-    ];
-
-    for (var index in expectedValues) {
-      assert.approximately(chromagram[index], expectedValues[index], 1e-15);
+    for (var i in TestData.EXPECTED_CHROMAGRAM_OUPUT) {
+      assert.approximately(chromagram[i], TestData.EXPECTED_CHROMAGRAM_OUPUT[i], 1e-15);
     }
 
     done();
