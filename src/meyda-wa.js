5ee177d5a481aac17e138fe41d5243602a74976e
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2018-11-01 13:44:48 +0100

    Allow setting number of mfcc coefficients in Analyzer constructor

--- src/meyda-wa.js
+++ src/meyda-wa.js
@@ -19,6 +19,7 @@ import * as featureExtractors from './featureExtractors';
   *   "bufferSize": 512,
   *   "featureExtractors": ["rms"],
   *   "inputs": 2,
+  *   "numberOfMFCCCoefficients": 20
   *   "callback": features => {
   *     levelRangeElement.value = features.rms;
   *   }
@@ -50,6 +51,7 @@ export class MeydaAnalyzer {
     this._m.windowingFunction = options.windowingFunction || 'hanning';
     this._m.featureExtractors = featureExtractors;
     this._m.EXTRACTION_STARTED = options.startImmediately || false;
+    this._m.numberOfMFCCCoefficients = options.numberOfMFCCCoefficients || this._m.numberOfMFCCCoefficients || 13;
     this._m.channel = typeof options.channel === 'number' ? options.channel : 0;
     this._m.inputs = options.inputs || 1;
     this._m.outputs = options.outputs || 1;
