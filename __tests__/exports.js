eef061543941612497c46ad812e7ebe9a9e43c87
Hugh Rawlinson <hughr2005@gmail.com>
2021-05-27 00:35:33 +0200

    fix(package exports): fix package exports properly this time
    Also, add tests to make sure that exports are working as expected
    
    fix #829

---
+++ __tests__/exports.js
@@ -0,0 +1,47 @@
+const EXPECTED_EXPORTS = [
+  "audioContext",
+  "spn",
+  "bufferSize",
+  "sampleRate",
+  "melBands",
+  "chromaBands",
+  "callback",
+  "windowingFunction",
+  "featureExtractors",
+  "EXTRACTION_STARTED",
+  "numberOfMFCCCoefficients",
+  "_featuresToExtract",
+  "windowing",
+  "_errors",
+  "createMeydaAnalyzer",
+  "extract",
+];
+
+describe("package exports", () => {
+  test("meyda web exports at least currently expected fields", () => {
+    expect(global.Meyda).not.toBeDefined();
+    var meyda = require("../dist/web/meyda");
+
+    expect(Object.keys(meyda)).toEqual(EXPECTED_EXPORTS);
+    expect(Object.keys(global.Meyda)).toEqual(EXPECTED_EXPORTS);
+    delete global.Meyda;
+  });
+
+  test("meyda web min exports at least currently expected fields", () => {
+    expect(global.Meyda).not.toBeDefined();
+    var meyda = require("../dist/web/meyda.min");
+
+    expect(Object.keys(meyda)).toEqual(EXPECTED_EXPORTS);
+    expect(Object.keys(global.Meyda)).toEqual(EXPECTED_EXPORTS);
+    delete global.Meyda;
+  });
+});
+
+describe("package exports", () => {
+  test("meyda node exports at least currently expected fields", () => {
+    var meyda = require("../dist/node/main");
+
+    expect(Object.keys(meyda)).toEqual(EXPECTED_EXPORTS);
+    expect(Object.keys(global.Meyda)).toEqual(EXPECTED_EXPORTS);
+  });
+});
