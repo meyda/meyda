0a15b960918d0322c8b3bb05e4b284d72c1cc3c3
Hugh Rawlinson <hughr2005@gmail.com>
2021-05-27 18:34:28 +0200

    docs(web bundling): clarify the purpose of indexjs as web entrypoint, not node entrypoint
    fix #832

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+/*
+This file is used as the entry point for only the web builds of Meyda. It is
+*not* used as the entrypoint for the node build of Meyda. It makes sure that
+Meyda directly exports itself on those builds, rather than the es6 default
+export. The entrypoint of the node build of Meyda is `main.js`.
+*/
 module.exports = require("./main").default;
