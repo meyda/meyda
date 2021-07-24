5afd4cad5c9f0abbe62018605d7d5481bf3c7d6c
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-24 18:22:36 +0200

    refactor(types): add spectral flux types

--- src/extractors/spectralFlux.ts
+++ src/extractors/spectralFlux.ts
@@ -1,17 +1,22 @@
 // This file isn't being typechecked at all because there are major issues with it.
 // See #852 for details. Once that's merged, this file should be typechecked.
 // @ts-nocheck
-export default function (args) {
-  if (
-    typeof args.signal !== "object" ||
-    typeof args.previousSignal != "object"
-  ) {
+export default function ({
+  signal,
+  previousSignal,
+  bufferSize,
+}: {
+  signal: Float32Array;
+  previousSignal: Float32Array;
+  bufferSize: number;
+}) {
+  if (typeof signal !== "object" || typeof previousSignal != "object") {
     throw new TypeError();
   }
 
   let sf = 0;
-  for (let i = -(args.bufferSize / 2); i < signal.length / 2 - 1; i++) {
-    x = Math.abs(args.signal[i]) - Math.abs(args.previousSignal[i]);
+  for (let i = -(bufferSize / 2); i < signal.length / 2 - 1; i++) {
+    x = Math.abs(signal[i]) - Math.abs(previousSignal[i]);
     sf += (x + Math.abs(x)) / 2;
   }
 
