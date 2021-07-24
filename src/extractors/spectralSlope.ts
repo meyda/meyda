d29fc6e113ade842ac92ed8f2cdba262481d88c5
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-24 18:20:47 +0200

    refactor(types): add spectral slope types

--- src/extractors/spectralSlope.ts
+++ src/extractors/spectralSlope.ts
@@ -1,26 +1,34 @@
-export default function (args) {
-  if (typeof args.ampSpectrum !== "object") {
+export default function ({
+  ampSpectrum,
+  sampleRate,
+  bufferSize,
+}: {
+  ampSpectrum: Float32Array;
+  sampleRate: number;
+  bufferSize: number;
+}) {
+  if (typeof ampSpectrum !== "object") {
     throw new TypeError();
   }
 
   //linear regression
   let ampSum = 0;
   let freqSum = 0;
-  let freqs = new Float32Array(args.ampSpectrum.length);
+  let freqs = new Float32Array(ampSpectrum.length);
   let powFreqSum = 0;
   let ampFreqSum = 0;
 
-  for (var i = 0; i < args.ampSpectrum.length; i++) {
-    ampSum += args.ampSpectrum[i];
-    let curFreq = (i * args.sampleRate) / args.bufferSize;
+  for (var i = 0; i < ampSpectrum.length; i++) {
+    ampSum += ampSpectrum[i];
+    let curFreq = (i * sampleRate) / bufferSize;
     freqs[i] = curFreq;
     powFreqSum += curFreq * curFreq;
     freqSum += curFreq;
-    ampFreqSum += curFreq * args.ampSpectrum[i];
+    ampFreqSum += curFreq * ampSpectrum[i];
   }
 
   return (
-    (args.ampSpectrum.length * ampFreqSum - freqSum * ampSum) /
+    (ampSpectrum.length * ampFreqSum - freqSum * ampSum) /
     (ampSum * (powFreqSum - Math.pow(freqSum, 2)))
   );
 }
