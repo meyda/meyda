e0a7931fa27884890285f52d7a29cd57a35322a1
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-24 17:40:51 +0200

    refactor(extractors): add arg types for rms

--- src/extractors/rms.ts
+++ src/extractors/rms.ts
@@ -1,14 +1,15 @@
-export default function (args) {
-  if (typeof args.signal !== "object") {
+export default function ({ signal }: { signal: Float32Array }) {
+  // Keeping this bad runtime typecheck for consistency
+  if (typeof signal !== "object") {
     throw new TypeError();
   }
 
   var rms = 0;
-  for (var i = 0; i < args.signal.length; i++) {
-    rms += Math.pow(args.signal[i], 2);
+  for (var i = 0; i < signal.length; i++) {
+    rms += Math.pow(signal[i], 2);
   }
 
-  rms = rms / args.signal.length;
+  rms = rms / signal.length;
   rms = Math.sqrt(rms);
 
   return rms;
