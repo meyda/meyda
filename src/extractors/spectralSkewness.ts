676c221610008da9ec2855c8a48ac2a1255678fb
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-24 18:19:14 +0200

    refactor(types): add spectral skewness types

--- src/extractors/spectralSkewness.ts
+++ src/extractors/spectralSkewness.ts
@@ -1,13 +1,13 @@
 import { mu } from "./extractorUtilities";
 
-export default function (args) {
-  if (typeof args.ampSpectrum !== "object") {
+export default function ({ ampSpectrum }: { ampSpectrum: Float32Array }) {
+  if (typeof ampSpectrum !== "object") {
     throw new TypeError();
   }
 
-  var mu1 = mu(1, args.ampSpectrum);
-  var mu2 = mu(2, args.ampSpectrum);
-  var mu3 = mu(3, args.ampSpectrum);
+  var mu1 = mu(1, ampSpectrum);
+  var mu2 = mu(2, ampSpectrum);
+  var mu3 = mu(3, ampSpectrum);
   var numerator = 2 * Math.pow(mu1, 3) - 3 * mu1 * mu2 + mu3;
   var denominator = Math.pow(Math.sqrt(mu2 - Math.pow(mu1, 2)), 3);
   return numerator / denominator;
