6e8f22fae2cb46e59cbfe0320e88e1c24267094d
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-25 15:49:40 +0200

    refactor(types): add spectral slope types

--- src/extractors/spectralSlope.ts
+++ src/extractors/spectralSlope.ts
@@ -6,7 +6,7 @@ export default function ({
   ampSpectrum: Float32Array;
   sampleRate: number;
   bufferSize: number;
-}) {
+}): number {
   if (typeof ampSpectrum !== "object") {
     throw new TypeError();
   }
