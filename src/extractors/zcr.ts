7dab8fbb9a5c3829969c72adfd601cd222bff3b6
Hugh Rawlinson <hughr2005@gmail.com>
2021-07-25 15:50:59 +0200

    refactor(types): add zcr types

--- src/extractors/zcr.ts
+++ src/extractors/zcr.ts
@@ -1,13 +1,13 @@
-export default function () {
-  if (typeof arguments[0].signal !== "object") {
+export default function ({ signal }: { signal: Float32Array }): number {
+  if (typeof signal !== "object") {
     throw new TypeError();
   }
 
   var zcr = 0;
-  for (var i = 1; i < arguments[0].signal.length; i++) {
+  for (var i = 1; i < signal.length; i++) {
     if (
-      (arguments[0].signal[i - 1] >= 0 && arguments[0].signal[i] < 0) ||
-      (arguments[0].signal[i - 1] < 0 && arguments[0].signal[i] >= 0)
+      (signal[i - 1] >= 0 && signal[i] < 0) ||
+      (signal[i - 1] < 0 && signal[i] >= 0)
     ) {
       zcr++;
     }
